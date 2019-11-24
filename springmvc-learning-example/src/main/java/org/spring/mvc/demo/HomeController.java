0e5a2dc23dc37138d37c5ce2039eb97e25eb323b
ChaoHappy <871761665@qq.com>
2019-11-24 11:33:47 +0800

    提交springmvc 示例代码

---
+++ springmvc-learning-example/src/main/java/org/spring/mvc/demo/HomeController.java
@@ -0,0 +1,16 @@
+package org.spring.mvc.demo;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/home")
+	public String showHomePage(Map<String, Object> model) {
+		model.put("spittles", "sssssssss");
+		return "home";
+	}
+}
